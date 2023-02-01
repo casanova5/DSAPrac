4ad006345803ee5cef3573e30d16be16d77e33e9
Purva Vijayvargiya <59122511+casanova5@users.noreply.github.com>
2023-02-01 15:57:08 +0530

    Create printallpairs.cpp

---
+++ Array/printallpairs.cpp
@@ -0,0 +1,33 @@
+/*
+i/p = arr[]={1,2,3,4}
+o/p = [(1,2) (1,3) (1,4) (2,3) (2,4) (3,4)]
+*/
+
+
+#include<iostream>
+using namespace std;
+
+void printSubarrays(int arr[],int n){
+	for(int i=0;i<n;i++){
+		for(int j=i+1; j<n;j++){
+
+		}
+
+	}
+
+}
+
+
+
+
+int main(){
+	//Array Containing 
+	int arr[] = {-2,3,4,5,-12,6,2,3};
+	int n = sizeof(arr)/sizeof(int);
+
+	printSubarrays(arr,n);
+
+
+
+	return 0;
+}
